4c17c2fb2f1006b4d6ffa595eae3c9d638bcda67
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-05-24 19:27:26 +0100

    Grid can be greated, and seeded randomly

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,10 +1,54 @@
-const rows = 5;
-const columns = 5;
+const rows = 10;
+const columns = 10;
+const seedQty = 20;
 
-for (let i = 0; i < rows; i++) {
-  let row = '';
-  for (let j = 0; j < columns; j++) {
-    row += '□ ';
+const createGrid = (rows, columns) => {
+  const columnArray = [];
+  for (let i = 0; i < rows; i++) {
+    const rowArray = [];
+    for (let j = 0; j < columns; j++) {
+      rowArray.push('□ ');
+    }
+    columnArray.push(rowArray);
   }
-  console.log(row);
-}
+  return columnArray;
+};
+
+const seedGrid = (qty, grid) => {
+  const livingCell = '■ ';
+  const emptyCells = []; // Store the indices of empty cells
+
+  // Find all the indices of empty cells
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '□ ') {
+        emptyCells.push({row: i, column: j});
+      }
+    }
+  }
+
+  // Randomly change qty number of empty cells to living cells
+  for (let i = 0; i < qty; i++) {
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const randomCell = emptyCells[randomIndex];
+    grid[randomCell.row][randomCell.column] = livingCell;
+    emptyCells.splice(randomIndex, 1);
+  }
+
+  return grid;
+};
+
+const printGrid = (grid) => {
+  for (let i = 0; i < grid.length; i++) {
+    console.log(grid[i].join(''));
+  }
+};
+
+// Create a 10x10 empty square grid
+const grid = createGrid(rows, columns);
+
+// Randomly change 20 cells into filled squares
+const filledGrid = seedGrid(seedQty, grid);
+
+// Print the filled grid
+printGrid(filledGrid);
